1ec0f19ba17062bf57c7e6d888705555ebf1c9c1
cdwilliams32 <44537294+cdwilliams32@users.noreply.github.com>
2021-01-13 14:43:03 -0600

    Added KOH. Still needs to be coded

---
+++ scripts/scenario_101_KOH.lua
@@ -0,0 +1,6 @@
+-- Name: King of the Hill
+-- Description: Every two minutes points are rewarded to the ships in the middle rings. 1 point for the outer, 2 for the middle and 3 for the inner. Ships are respawned every two minutes as long as their team base is intact.
+-- Type: Basic
+
+--- Scenario
+-- @script scenario_101_KOH
