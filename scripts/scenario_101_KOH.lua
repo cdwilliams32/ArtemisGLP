0e0fc151638d2fcb1bc1b8a8bd28dc3a8fd67584
cdwilliams32 <44537294+cdwilliams32@users.noreply.github.com>
2021-01-13 15:16:44 -0600

    Added layout
    Will finish tomorrow. Want to cuddle with my dog

--- scripts/scenario_101_KOH.lua
+++ scripts/scenario_101_KOH.lua
@@ -4,3 +4,56 @@
 
 --- Scenario
 -- @script scenario_101_KOH
+
+--run at start
+function init()
+    --generate tables
+    playerShips = {}
+    spaceStations = {}
+
+    --spawn world
+    spawnWorld()
+    --spawn zones
+    spawnZones()
+    --spawn ships
+    spawnShips()
+end
+
+--updates every 1 millisecond
+function update(delta)
+end
+
+--spawns the spacestations and artifacts
+function spawnWorld()
+    --inner ring
+    Artifact():setModel("artifact8"):setPosition(0,1750)
+    Artifact():setModel("artifact8"):setPosition(0,-1750)
+    Artifact():setModel("artifact8"):setPosition(1750,0)
+    Artifact():setModel("artifact8"):setPosition(-1750,0)
+
+    Artifact():setModel("artifact8"):setPosition(-1237.44,1237.44)
+    Artifact():setModel("artifact8"):setPosition(1237.44,1237.44)
+    Artifact():setModel("artifact8"):setPosition(1237.44,-1237.44)
+    Artifact():setModel("artifact8"):setPosition(-1237.44,1237.44)
+
+    --middle ring
+end
+
+--spawn the zones for play area and point rewarding
+function spawnZones()
+end
+
+--spawn player ships
+function spawnShips()
+    Alpha1 = PlayerSpaceShip():setFaction("Alpha"):setCallsign("Alpha1"):setTemplate("Atlantis")
+    Alpha2
+
+    Bravo1
+    Bravo2
+
+    Charlie1
+    Charlie2
+
+    Delta1
+    Delta2
+end
