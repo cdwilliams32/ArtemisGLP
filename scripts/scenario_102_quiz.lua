f7755812efc7af83a31f921f56fe8b5301ffd004
cdwilliams32 <44537294+cdwilliams32@users.noreply.github.com>
2021-01-15 15:38:12 -0600

    Added quiz
    Just a layout.

---
+++ scripts/scenario_102_quiz.lua
@@ -0,0 +1,12 @@
+-- Name: Quiz
+-- Description: Help understand how Empty Epsilon works.
+
+--- Scenario
+-- @script scenario_102_quiz
+
+--run at start
+function init()
+end
+
+function update(delta)
+end
